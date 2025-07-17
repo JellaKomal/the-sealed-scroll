989f3790ff1d05855d9f0e4ab5acb76f2b6f4f49
JellaKomal <137670169+JellaKomal@users.noreply.github.com>
2025-07-17 14:23:40 +0000

    feat: add Alert components with customizable variants and styles

--- the-kunai/src/App.tsx
+++ the-kunai/src/App.tsx
@@ -1,22 +1,11 @@
-import { Clock } from 'lucide-react'
 import './App.css'
-import { Accordion } from './components/design-system/accordion'
 
 function App() {
 
   return (
-    <>
-      <Accordion
-        id='bottom-border-accordion'
-        title="Accessible FAQ Sectionbnm"
-        triggerPosition="left"
-        titleIcon={<Clock size={20} />}
-        variant="bottom-border"
-        content={<p>This content is accessible and WCAG-compliant.</p>}
-        defaultOpen
-      />
+    <div className='p-4'>
 
-    </>
+    </div>
   )
 }
 
